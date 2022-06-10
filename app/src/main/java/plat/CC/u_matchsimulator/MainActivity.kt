ffb04f7b52c56f22efb7cd753d000bacf4ec949b
Cricio de Deus Oliveira <cricio1oliveira@gmail.com>
2022-06-09 23:56:08 -0300

    first commit

---
+++ app/src/main/java/plat/CC/u_matchsimulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package plat.CC.u_matchsimulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
