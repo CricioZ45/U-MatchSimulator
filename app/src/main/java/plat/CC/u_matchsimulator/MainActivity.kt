b33359d2699991e9cd8921fee475c89f778545de
Cricio de Deus Oliveira <cricio1oliveira@gmail.com>
2022-06-13 14:59:22 -0300

    Principal mudança: adição das cores primaria e secundaria e a crição de um ícone para o aplicativo.

--- app/src/main/java/plat/CC/u_matchsimulator/MainActivity.kt
+++ app/src/main/java/plat/CC/u_matchsimulator/MainActivity.kt
@@ -1,11 +1,18 @@
 package plat.CC.u_matchsimulator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import plat.CC.u_matchsimulator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
